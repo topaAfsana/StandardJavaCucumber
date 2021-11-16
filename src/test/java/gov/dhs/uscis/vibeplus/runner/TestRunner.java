8e81bc7bc867aa1ac3787cc7ae492d125459a93a
MD PARVESH <abraartishan@Mds-MacBook-Pro.local>
2021-11-16 10:56:54 -0500

    jdk

--- src/test/java/gov/dhs/uscis/vibeplus/runner/TestRunner.java
+++ src/test/java/gov/dhs/uscis/vibeplus/runner/TestRunner.java
@@ -16,3 +16,24 @@ import io.cucumber.junit.CucumberOptions;
 
 public class TestRunner {
 }
+
+
+
+//ISSUE:JRE/JDK/COMPILER ISSUE
+//
+//SOLUTION:
+//1.CHANGE BUILD PATH IN ECLIPSE JRE TO JDK
+//       ADD JDK AS  ALTERNATE INSTALLED ONE AND AS EXECUTE JDK
+//2.ADD SETTINGS FILE TO .M2 FOLDER
+//        UNDER USER FOLDER
+//3.MAVEN COMPILER PLUG IN TO RIGHT JAVA VERSION 1.8 OR SO
+//IN POM FILE PLUG IN SECTION
+//<plugin>
+//<groupId>org.apache.maven.plugins</groupId>
+//<artifactId>maven-compiler-plugin</artifactId>
+//<version>3.8.1</version>
+//<configuration>
+//<source>1.8</source>
+//<target>1.8</target>
+//</configuration>
+//</plugin>
